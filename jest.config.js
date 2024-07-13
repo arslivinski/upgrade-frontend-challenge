19ad0aae0ea7fd6bdbb3b1881b7866b941955d25
Anderson Slivinski <11060804+arslivinski@users.noreply.github.com>
2024-07-13 14:18:35 -0300

    chore: extract configurations from package.json

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @import { Config } from 'jest' */
+
+/**
+ * @see https://jestjs.io/docs/configuration
+ * @type {Config}
+ */
+const config = {
+  testEnvironment: 'jsdom',
+};
+
+export default config;
